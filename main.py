574f5e28ea34297a0b814c5d8677d4e0481efc1a
Ajax Gunterson <Ajax@AjaxGunterson.com>
2023-10-09 20:32:26 -0500

    Works now and has a maximum shutdown time variable

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import random
 from datetime import datetime
 import time
 
+MAX_SHUTDOWN_TIME = 180#max shutdown time in minutes
+
 class youtubeVideo:
     def __init__(self, videoID, videoLength):
       self.videoID = videoID,
@@ -25,7 +27,7 @@ def remove_non_time(inputList):
 def remove_duplicates(inputList):
    filteredList = []
    for i, input in enumerate(inputList):
-      if (i % 2) == 0:
+      if (i % 2) == 1:
          filteredList.append(input)
    return filteredList
 
@@ -41,7 +43,6 @@ def time_to_sec(times):
 
 baseChannelURL = "https://www.youtube.com/@{REPLACE_CHANNEL}/videos"
 videoURL = "https://www.youtube.com/watch?v="
-shutdownTime = 60
 
 artistUsername = "JermaStreamArchive"
 channelURL = baseChannelURL.replace("{REPLACE_CHANNEL}", artistUsername)
@@ -51,7 +52,7 @@ channelResponse = requests.get(channelURL)
 
 
 channelResults = re.findall(r'url\"\:\"\/watch\?v\=(.*?(?=\"))', channelResponse.text)
-channelTimes = re.findall(r'ds\"\}\},\"simpleText\":\"(.*?(?=\"))', channelResponse.text) #simpleText\":\"(.*?(?=\"))
+channelTimes = re.findall(r'simpleText\":\"(.*?(?=\"))', channelResponse.text) #ds\"\}\},\"simpleText\":\"(.*?(?=\"))
 channelTimes = remove_non_time(channelTimes)
 channelTimes = remove_duplicates(channelTimes)
 channelTimes = time_to_sec(channelTimes)
@@ -63,13 +64,17 @@ if platform == "linux" or platform == "linux2":
 elif platform == "darwin":
     shutdownCommand = "shutdown -s +"
 elif platform == "win32":
-    shutdownTime *= 60
     shutdownCommand = "shutdown /s /t "
     
 for i, result in enumerate(channelResults):
-   videoList.append(youtubeVideo(result, str(channelTimes[i].videoLength)))
+    if channelTimes[i] > MAX_SHUTDOWN_TIME:
+       channelTimes[i] = MAX_SHUTDOWN_TIME
+    if platform == "win32":
+      videoList.append(youtubeVideo(result, channelTimes[i] * 60))
+    else:
+        videoList.append(youtubeVideo(result, channelTimes[i]))
 
 if len(channelResults) != 0:
   vidIndex = random.randint(0, len(videoList) - 1)
   webbrowser.open(videoURL + (str)(videoList[vidIndex].videoID[0]), new=0, autoraise=True)
-  os.system(shutdownCommand + str(videoList[vidIndex].videoLength))
+  os.system(shutdownCommand + (str)(videoList[vidIndex].videoLength))
